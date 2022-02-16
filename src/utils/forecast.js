d3023713573c4449b4d35bca57b30cd49ebb383b
Rafael Bellintani <chaulingo1@gmail.com>
2022-02-16 09:49:58 -0300

    Update forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,13 +1,5 @@
 const request = require('postman-request')
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
+
 const access_token = 'b07c99d20bf4cb56a85a60cc6962829c'
 
 const forecast = (latitude, longitude, callback) => {
